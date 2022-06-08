257c6bcd85015de006ac73e662cf1f02eaeee335
mahi0898 <98941426+mahi0898@users.noreply.github.com>
2022-06-08 17:17:55 +0530

    Update upload artifact.sh

--- upload artifact.sh
+++ upload artifact.sh
@@ -1,3 +1,8 @@
 set -e
 set -x
+      - name: uploading zip
+        uses: actions/upload-artifact@v3
+        with:
+          name: SmokeTest
           path: /Users/runner/work/splunk-otel-ios/splunk-otel-ios/SmokeTest.zip
+
