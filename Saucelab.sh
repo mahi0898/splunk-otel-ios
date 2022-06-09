cfa2704da249f489308bb2f5960f031feeb96006
mahi0898 <98941426+mahi0898@users.noreply.github.com>
2022-06-09 10:29:16 +0530

    Update Saucelab.sh

--- Saucelab.sh
+++ Saucelab.sh
@@ -1,4 +1,7 @@
-          cd /Users/runner/work/splunk-otel-ios/splunk-otel-ios/
+set -e
+set -x
+
+       cd /Users/runner/work/splunk-otel-ios/splunk-otel-ios/
           ls
           curl -u "sso-splunk.saucelabs.com-mahimag:274c9a94-86d1-4b12-9594-57307cfb2c57" --location \
              --request POST 'https://api.us-west-1.saucelabs.com/v1/storage/upload' \
