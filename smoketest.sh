547582538cbace4163ca7910fe2d9489b66462bc
mahi0898 <98941426+mahi0898@users.noreply.github.com>
2022-06-13 16:11:27 +0530

    Update smoketest.sh

--- smoketest.sh
+++ smoketest.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 set -e
 set -x
-env: 
-         destination: ${{ matrix.destination }}
 
 # This gobbledygook gets a list of simulator iPhones and picks the id of the first one
 # (different samples of the output of this have [] or () as delimiters of the last (id) field)
 TEST_DEVICE=`xcrun xctrace list devices 2>&1  | grep iPhone | head -1  | sed 's/.*[([]//' | sed 's/.$//'`
 
-xcodebuild -workspace SplunkRumWorkspace/SplunkRumWorkspace.xcworkspace -scheme SmokeTest -configuration Debug test - echo $ destination
+xcodebuild -workspace SplunkRumWorkspace/SplunkRumWorkspace.xcworkspace -scheme SmokeTest -configuration Debug test - $ echo "${{matrix.destination}}"
 zip ${GITHUB_WORKSPACE}/SmokeTest.zip $(find /Users/runner/Library/Developer/Xcode/DerivedData/SplunkRumWorkspace-dzmkiduinguystddyqrorblmaahj/Build/Products/Debug-iphonesimulator/SmokeTest.app -type f)
 pwd
           
