54563ea0f3a1ed2226dcd731dde7d618514fe687
mahi0898 <98941426+mahi0898@users.noreply.github.com>
2022-06-21 18:36:11 +0530

    Update test_appium.py

--- SplunkRumWorkspace/SmokeTest/test_appium.py
+++ SplunkRumWorkspace/SmokeTest/test_appium.py
@@ -44,7 +44,7 @@ class HybridIOSTests(unittest.TestCase):
             
     #Click on Configure disableMemoryWarning and Test disableMemoryWarning
     def test_API_DisableMemoryWarning(self):
-        self.driver.find_element_by_id("CLICK ME").click();
+        self.driver.find_element_by_id("CLICK ME")
            if self.is_displayed():
          self.click()
          else
