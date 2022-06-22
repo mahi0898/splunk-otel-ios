63174d300df0907a64f03daca3ab184f3cc58ebc
mahi0898 <98941426+mahi0898@users.noreply.github.com>
2022-06-22 15:35:05 +0530

    Create test_appium.py

--- SplunkRumWorkspace/SmokeTest/test_appium.py
+++ SplunkRumWorkspace/SmokeTest/test_appium.py
@@ -44,8 +44,7 @@ class HybridIOSTests(unittest.TestCase):
             
     #Click on Configure disableMemoryWarning and Test disableMemoryWarning
     def test_API_DisableMemoryWarning(self):
-        self.driver.find_element_by_id("CLICK ME").click()
-        assert self.driver.find_element_by_id("ABC").click(), "Failed"
+        assert self.driver.find_element_by_id("CLICK ME").click()  
         #AssertionError: True is not false : testvalue is not false
 
 if __name__ == "__main__":
