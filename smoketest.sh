a6d166b89a5ae873081467c527caefc37c24e687
mahi0898 <98941426+mahi0898@users.noreply.github.com>
2022-06-23 15:54:08 +0530

    Update smoketest.sh

--- smoketest.sh
+++ smoketest.sh
@@ -5,10 +5,12 @@ set -x
 # This gobbledygook gets a list of simulator iPhones and picks the id of the first one
 # (different samples of the output of this have [] or () as delimiters of the last (id) field)
 TEST_DEVICE=`xcrun xctrace list devices 2>&1  | grep iPhone | head -1  | sed 's/.*[([]//' | sed 's/.$//'`
+BUILD_FOLDER="work/splunk-otel-ios/splunk-otel-ios/SmokeBuild"
+BUILD_PATH="Build/Products/Debug-iphonesimulator"
+BUILD_NAME="SmokeTest.app"
 
-xcodebuild -workspace SplunkRumWorkspace/SplunkRumWorkspace.xcworkspace -scheme SmokeTest -configuration Debug test -destination platform="iOS Simulator,id=$TEST_DEVICE"
-
-zip ${GITHUB_WORKSPACE}/SmokeTest.zip $(find /Users/runner/Library/Developer/Xcode/DerivedData/SplunkRumWorkspace-dzmkiduinguystddyqrorblmaahj/Build/Products/Debug-iphonesimulator/SmokeTest.app -type f)
+xcodebuild -workspace SplunkRumWorkspace/SplunkRumWorkspace.xcworkspace -scheme SmokeTest -configuration Debug test -destination platform="iOS Simulator,id=$TEST_DEVICE" -derivedDataPath SmokeBuild
+zip ${GITHUB_WORKSPACE}/SmokeTest.zip $(find ~/$BUILD_FOLDER/$BUILD_PATH/$BUILD_NAME -type f)
 pwd
 
           
